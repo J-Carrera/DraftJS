c40afa7f698e1bf78832d5af9b836cfd1ecc02ba
John Carrera <john.carlos.carrera@gmail.com>
2025-07-12 20:26:45 -0400

    DraftJS Rich Text Editor

---
+++ src/components/RichTextEditor.js
@@ -0,0 +1,186 @@
+import React from "react";
+import {
+  Editor,
+  EditorState,
+  getDefaultKeyBinding,
+  RichUtils,
+  convertToRaw,
+} from "draft-js";
+import "./RichTextEditor.css";
+import "../../node_modules/draft-js/dist/Draft.css";
+import "../App.css";
+import axios from "axios";
+import draftToHtml from "draftjs-to-html";
+
+class RichTextEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.editorRef = React.createRef();
+
+    this.focus = () => this.editorRef.current.focus();
+    this.onChange = (editorState) => this.setState({ editorState });
+
+    this.handleKeyCommand = this._handleKeyCommand.bind(this);
+    this.mapKeyToEditorCommand = this._mapKeyToEditorCommand.bind(this);
+    this.toggleBlockType = this._toggleBlockType.bind(this);
+    this.toggleInlineStyle = this._toggleInlineStyle.bind(this);
+  }
+
+  _handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
+  }
+
+  _mapKeyToEditorCommand(e) {
+    if (e.keyCode === 9 /* TAB */) {
+      const newEditorState = RichUtils.onTab(
+        e,
+        this.state.editorState,
+        4 /* maxDepth */
+      );
+      if (newEditorState !== this.state.editorState) {
+        this.onChange(newEditorState);
+      }
+      return;
+    }
+    return getDefaultKeyBinding(e);
+  }
+
+  _toggleBlockType(blockType) {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, blockType));
+  }
+
+  _toggleInlineStyle(inlineStyle) {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, inlineStyle)
+    );
+  }
+
+  render() {
+    const { editorState } = this.state;
+
+    let className = "RichEditor-editor";
+    var contentState = editorState.getCurrentContent();
+    if (!contentState.hasText()) {
+      if (contentState.getBlockMap().first().getType() !== "unstyled") {
+        className += " RichEditor-hidePlaceholder";
+      }
+    }
+
+    // API does not currently work, endpoint is an example not functional
+    const sendToApi = async () => {
+      const content = convertToRaw(editorState.getCurrentContent());
+      try {
+        await axios.put("https://your-pythonanywhere-api.com/endpoint", {
+          content,
+        });
+        alert("Content sent to API successfully!");
+      } catch (error) {
+        alert("Failed to send content to API: " + error.message);
+      }
+    };
+
+    const HtmlAlert = () => {
+      const rawContentState = convertToRaw(editorState.getCurrentContent());
+      const markup = draftToHtml(rawContentState);
+      alert(markup);
+    };
+
+    return (
+      <div className="RichEditor-root">
+        <InlineStyleControls
+          editorState={editorState}
+          onToggle={this.toggleInlineStyle}
+        />
+        <div className={className} onClick={this.focus}>
+          <Editor
+            blockStyleFn={getBlockStyle}
+            customStyleMap={styleMap}
+            editorState={editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            keyBindingFn={this.mapKeyToEditorCommand}
+            onChange={this.onChange}
+            placeholder="Tell a story..."
+            ref={this.editorRef}
+            spellCheck={true}
+          />
+        </div>
+        <button onClick={HtmlAlert}>HTML</button>
+        <button onClick={sendToApi}>API</button>
+      </div>
+    );
+  }
+}
+
+// Custom overrides for "code" style.
+const styleMap = {
+  CODE: {
+    backgroundColor: "rgba(0, 0, 0, 0.05)",
+    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+    fontSize: 16,
+    padding: 2,
+  },
+};
+
+function getBlockStyle(block) {
+  switch (block.getType()) {
+    case "blockquote":
+      return "RichEditor-blockquote";
+    default:
+      return null;
+  }
+}
+
+class StyleButton extends React.Component {
+  constructor() {
+    super();
+    this.onToggle = (e) => {
+      e.preventDefault();
+      this.props.onToggle(this.props.style);
+    };
+  }
+
+  render() {
+    let className = "RichEditor-styleButton";
+    if (this.props.active) {
+      className += " RichEditor-activeButton";
+    }
+
+    return (
+      <span className={className} onMouseDown={this.onToggle}>
+        {this.props.label}
+      </span>
+    );
+  }
+}
+
+var INLINE_STYLES = [
+  { label: "Bold", style: "BOLD" },
+  { label: "Italic", style: "ITALIC" },
+  { label: "Underline", style: "UNDERLINE" },
+];
+
+const InlineStyleControls = (props) => {
+  const currentStyle = props.editorState.getCurrentInlineStyle();
+
+  return (
+    <div className="RichEditor-controls">
+      {INLINE_STYLES.map((type) => (
+        <StyleButton
+          key={type.label}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RichTextEditor;
